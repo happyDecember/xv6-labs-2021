6d7e4381475125c6aa2ee4207f31cece87dc4176
happyDecember <152608184+happyDecember@users.noreply.github.com>
2024-08-03 20:07:44 +0800

    上传了实验三

---
+++ lab-pgtbl-handin/kernel/sysproc.c
@@ -0,0 +1,128 @@
+#include "types.h"
+#include "riscv.h"
+#include "param.h"
+#include "defs.h"
+#include "date.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+
+
+uint64
+sys_exit(void)
+{
+  int n;
+  if(argint(0, &n) < 0)
+    return -1;
+  exit(n);
+  return 0;  // not reached
+}
+
+uint64
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+  return fork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  if(argaddr(0, &p) < 0)
+    return -1;
+  return wait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+uint64
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+
+#ifdef LAB_PGTBL
+int
+sys_pgaccess(void)
+{
+  // lab pgtbl: your code here.
+  uint64 address, mask_address;
+  argaddr(0, &address);
+  argaddr(2, &mask_address);
+  int len;
+  argint(1, &len);
+  if(len > 32)
+    return -1;
+  struct proc* proc = myproc();
+  uint32 mask = 0;
+  for(int i = 0; i < len; i++){
+    pte_t* pte = walk(proc->pagetable, address + i * PGSIZE, 0);
+    if(*pte & PTE_A){
+	mask |= 1 << i;  // 将当前虚拟地址对应的位设置为1
+        *pte &= ~PTE_A;  // 重置当前页表项的页面访问位
+    }
+  } 
+  if(copyout(proc->pagetable, mask_address, (char*)&mask, 4) < 0)
+    return -1;	  
+  return 0;
+}
+#endif
+
+uint64
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+uint64
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
